9a6daa5237955bfe57f6eaff29dbaa59818e229b
Hunter Key <hunterjkey@gmail.com>
2019-01-04 08:55:20 -0800

    Update HunterKeyIA.py

--- HunterKeyIA.py
+++ HunterKeyIA.py
@@ -33,14 +33,24 @@ bump = False
 bumpwidth = 400
 Cycle = 0
 Counts = 0
+slidersY = 215
+notchesY = 0
+notchesX = 0
+
 MolesLetters = ["M","O","L","E","S"]
+MolesLettersX = [15,15,16,16,16]
 PressureLetters = ["P","R","E","S","S","U","R","E"]
+PressureLettersX = [16,15,16,16,16,15,15,16]
 TemperatureLetters = ["T","E","M","P","E","R","A","T","U","R","E"]
+TemperatureLettersX = [16,16,15,16,16,15,15,16,15,15,16]
 VolumeLetters = ["V","O","L","U","M","E"]
+VolumeLettersX = [15,15,16,15,15,16]
 letterY = 185 #Y position for the column letters
 letterX = 15 #Y position for the column letters
 LettersList = [MolesLetters, PressureLetters, TemperatureLetters, VolumeLetters]
+LettersListX = [MolesLettersX, PressureLettersX, TemperatureLettersX, VolumeLettersX]
 LettersChoice = 0 #current column being printed
+LettersChoiceX = 0 #current column's positions being printed
 
 
 
@@ -72,11 +82,11 @@ while not done:
             if event.key == pygame.K_s:
                 if VolumeAmount>0:
                     VolumeAmount-=10
-                for i in range(len(MolesX)):
-                    if (MolesX[i]-175>15):
-                        MolesX[i]-=10
-                    if (MolesY[i]-180>15):
-                        MolesY[i]-=10
+                    for i in range(len(MolesX)):
+                        if (MolesX[i]-175>15):
+                            MolesX[i]-=10
+                        if (MolesY[i]-180>15):
+                            MolesY[i]-=10
 
     #screen fill
     screen.fill(BLACK)
@@ -157,22 +167,42 @@ while not done:
     pygame.draw.rect(screen, WHITE, [(130, 180), (30, 300)])
     pygame.draw.rect(screen, WHITE, [(175-bumpwidth, 180-bumpwidth), (300+(2*bumpwidth)+VolumeAmount, 200+(2*bumpwidth)+VolumeAmount)], 3)
     pygame.draw.rect(screen, WHITE, [(600, 180), (185, 300)], 3)
+    for i in range(4):
+        pygame.draw.line(screen, WHITE, [615, slidersY], [765, slidersY], 3)
+        slidersY += 70
+    slidersY = 225
+
+    for i in range(4):
+        for i in range(11):
+            pygame.draw.line(screen, WHITE, [notchesX, notchesY], [notchesX, notchesY+10], 3)
+            notchesX += 15
+        notchesX = 615
+        notchesY += 70
+    notchesY = 220
+
+    notchesY = 215
+    for i in range(4):
+        pygame.draw.line(screen, RED, [690, notchesY], [690, notchesY+20], 5)
+        notchesY += 70
+    notchesY = 220
+
+    pygame.draw.rect(screen, BLUE, [(15, 480-(MoleAmount*30)), (20, (MoleAmount*30))])
+    pygame.draw.rect(screen, ORANGE, [(55, 480-(PressureAmount*5)), (20, (PressureAmount*5))])
+    pygame.draw.rect(screen, RED, [(95, 480-(TempAmount*30)), (20, (TempAmount*30))])
+    pygame.draw.rect(screen, GREEN, [(135, 480-(VolumeAmount*3)), (20, (VolumeAmount*3))])
 
     #-Column Code-
     font = pygame.font.SysFont('timesnewroman', 24, False, False)
     for a in range(len(LettersList)):
         LettersChoice = LettersList[a]
+        LettersChoiceX = LettersListX[a]
         for i in range(len(LettersChoice)):
-            text = font.render(LettersChoice[i],True,RED)
-            screen.blit(text, [letterX, letterY])
+            text = font.render(LettersChoice[i],True,BLACK)
+            screen.blit(text, [letterX+LettersChoiceX[i], letterY])
             letterY += 20
-        letterX += 30
+        letterY = 185
+        letterX += 40
     letterX = 0
-    
-    pygame.draw.rect(screen, BLUE, [(15, 480-(MoleAmount*30)), (20, (MoleAmount*30))])
-    pygame.draw.rect(screen, ORANGE, [(55, 480-(PressureAmount*5)), (20, (PressureAmount*5))])
-    pygame.draw.rect(screen, RED, [(95, 480-(TempAmount*30)), (20, (TempAmount*30))])
-    pygame.draw.rect(screen, GREEN, [(135, 480-(VolumeAmount*3)), (20, (VolumeAmount*3))])
 
     for i in range(len(MolesX)):
         pygame.draw.ellipse(screen, WHITE, [MolesX[i],MolesY[i],25,25], 3)
